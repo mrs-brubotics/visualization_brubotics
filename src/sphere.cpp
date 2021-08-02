ec0a993adc43d637a4db24b1f30ed83e957b9bee
TitouanTyack <tyacktitouan@gmail.com>
2021-08-02 18:00:22 +0200

    two sphere on drones

--- src/sphere.cpp
+++ src/sphere.cpp
@@ -1,26 +1,33 @@
 #include <visualization_msgs/Marker.h>
 #include <ros/ros.h>
+#include <mrs_msgs/FutureTrajectory.h>
+#include <iostream>
+#include <geometry_msgs/PoseArray.h>
+#include <geometry_msgs/Pose.h>
+#include <string>
+
+void pose_callback(const geometry_msgs::PoseArray::ConstPtr& msg, int uav_number);
+void publishSphereMarker(const geometry_msgs::PoseArray& obj_pose, int uav_number);
 
 
+double obst_sphere1_radius_ = 1.5;
 
-int main(int argc, char **argv){
-    
-    // Initialization
-    // ^^^^^^^^^^^^^^
+ros::Publisher marker_sphere1_pub;
+ros::Publisher marker_sphere2_pub;
+ros::Subscriber pose_subscriber1;
+ros::Subscriber pose_subscriber2;
 
-    // init node
-    ros::init(argc, argv,"marker");
-    ros::NodeHandle n;
+geometry_msgs::PoseArray uav_pose;
 
-    ros::Publisher marker_sphere1_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+void pose_callback(const geometry_msgs::PoseArray::ConstPtr& msg, int uav_number){
+    uav_pose.poses = msg -> poses;
+    ROS_INFO_STREAM("camera pos:\nx: " << uav_number);
+    publishSphereMarker(uav_pose, uav_number);
+}
 
-    double obst_sphere1_radius_ = 10;
-    double obst_sphere1_center_x = 0.3;
-    double obst_sphere1_center_y = 0.3;
-    double obst_sphere1_center_z = 0.65;
-    
-    while (ros::ok()){
 
+void publishSphereMarker(const geometry_msgs::PoseArray& obj_pose, int uav_number){
+    ROS_INFO_STREAM("ok4");
     visualization_msgs::Marker marker_sphere1;
     marker_sphere1.header.frame_id = "/common_origin";
     marker_sphere1.header.stamp = ros::Time::now();
@@ -28,25 +35,53 @@ int main(int argc, char **argv){
     marker_sphere1.id = 0;
     marker_sphere1.type = visualization_msgs::Marker::SPHERE; 
     marker_sphere1.action = visualization_msgs::Marker::ADD;
-    marker_sphere1.pose.position.x = obst_sphere1_center_x; 
-    marker_sphere1.pose.position.y = obst_sphere1_center_y; 
-    marker_sphere1.pose.position.z = obst_sphere1_center_z;  
-    marker_sphere1.pose.orientation.x = 0.0;
-    marker_sphere1.pose.orientation.y = 0.0;
-    marker_sphere1.pose.orientation.z = 0.0;
-    marker_sphere1.pose.orientation.w = 1.0;
+
+    //for fixed sphere
+    // marker_sphere1.pose.position.x = 5.0; 
+    // marker_sphere1.pose.position.y = 5.0; 
+    // marker_sphere1.pose.position.z = 5.0;  
+    // marker_sphere1.pose.orientation.x = 0.0;
+    // marker_sphere1.pose.orientation.y = 0.0;
+    // marker_sphere1.pose.orientation.z = 0.0;
+    // marker_sphere1.pose.orientation.w = 1.0;
+
     marker_sphere1.scale.x = 2*obst_sphere1_radius_; 
     marker_sphere1.scale.y = 2*obst_sphere1_radius_; 
     marker_sphere1.scale.z = 2*obst_sphere1_radius_; 
     marker_sphere1.color.r = 1.0f;
     marker_sphere1.color.g = 0.0f;
     marker_sphere1.color.b = 0.0f;
-    marker_sphere1.color.a = 0.8;
+    marker_sphere1.color.a = 0.25;
     marker_sphere1.lifetime = ros::Duration();
-    marker_sphere1_pub.publish(marker_sphere1);
+    if(uav_number == 1){
+        marker_sphere1.pose = obj_pose.poses[0];
+        marker_sphere1_pub.publish(marker_sphere1);
     }
+    if(uav_number == 2){
+        marker_sphere1.pose = obj_pose.poses[0];
+        marker_sphere2_pub.publish(marker_sphere1);
+    }
+}
 
+int main(int argc, char **argv){
+    
+    // Initialization
+
+    // init node
+    ros::init(argc, argv,"marker");
+    ros::NodeHandle n;
+
+    // create subscriber for uav_predicted pose
+    boost::function<void (const geometry_msgs::PoseArray::ConstPtr&)> f1 = boost::bind(pose_callback, _1, 1);
+    pose_subscriber1 = n.subscribe("/uav1/control_manager/dergbryan_tracker/custom_predicted_poses", 10, f1);
+
+    boost::function<void (const geometry_msgs::PoseArray::ConstPtr&)> f2 = boost::bind(pose_callback, _1, 2);
+    pose_subscriber2 = n.subscribe("/uav2/control_manager/dergbryan_tracker/custom_predicted_poses", 10, f2);
+    
+    // create publisher for marker
+    marker_sphere1_pub = n.advertise<visualization_msgs::Marker>("visualization_marker1", 10);
+    marker_sphere2_pub = n.advertise<visualization_msgs::Marker>("visualization_marker2", 10);
+
+    ros::spin();
     return 0;
-
-
 }
